c99694c8d86f82e79d547027bd298fc5d19329c7
James Grenning <james.grenning.net>
2017-11-07 11:23:11 -0600

    Changed port to 'SnapStick0', also did a reboot recently

--- src/hub/environment/platform.py
+++ src/hub/environment/platform.py
@@ -15,7 +15,8 @@ class HubPlatform:
             log.info("Mac")
             # SS200 USB stick on mac
             self.serial_conn = snap.SERIAL_TYPE_RS232
-            self.serial_port = '/dev/tty.SLAB_USBtoUART'
+            self.serial_port = 'SnapStick0'
+            # self.serial_port = '/dev/tty.SLAB_USBtoUART'
         elif self.platform == "win32":
             log.info("Windows")
             # SS200 USB stick on Windows
