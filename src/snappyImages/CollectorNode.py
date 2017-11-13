9a5e7c27728db45a259f4b893d4138d0122f7240
James Grenning <james.grenning.net>
2017-11-13 12:04:34 -0600

    Cleaned up Collector.  using signal stretcher

--- src/snappyImages/CollectorNode.py
+++ src/snappyImages/CollectorNode.py
@@ -1,20 +1,56 @@
 from utils.binhex import hexValues3
 from utils.spi7191 import *
 
-readPending = False
 hub_addr = None
+read_initiated = False
 
 @setHook(HOOK_STARTUP)
 def startup():
-    global read_initiated
-    read_initiated = False
-    global readPending
-    readPending = False
     mcastRpc(1,2,"imAlive")
     print "imAlive"
     snappySpiInit()
     initMonitorAdcReady()
     snappySpiRead(3, 8)
+
+ADC_READY_PIN = 30
+ADC_READY = False
+EVERY_MS = 3
+def initMonitorAdcReady():
+    setPinDir(ADC_READY_PIN, False)
+    monitorPin(ADC_READY_PIN, True)
+    setRate(EVERY_MS)
+
+@setHook(HOOK_GPIN)
+def pinChanged(pinNum, isSet):
+    global read_initiated
+    if not read_initiated:
+        return
+    if pinNum == ADC_READY_PIN and isSet == ADC_READY:
+        readAndSend()
+
+# -- Collector node API functions
+
+def enableCollector():
+    snappyADCEnable()
+    addr = rpcSourceAddr()
+    sendAck(addr, "enableCollector", "-")
+
+def disableCollector():
+    snappyADCDisable()
+    addr = rpcSourceAddr()
+    global read_initiated
+    read_initiated = False
+    sendAck(addr, "disableCollector", "-")
+
+def initiateRead():
+    global read_initiated
+    read_initiated = True
+    global hub_addr
+    hub_addr = rpcSourceAddr()
+    sendAck(hub_addr, "readInitiated", "-")
+
+# -- Helpers ---
+
 
 def sendAck(addr, command, result):
     rpc(addr, "ack", command, result)
@@ -34,48 +70,8 @@ def announceHub():
 def three_bytes_as_text(addr):
     return hexValues3(ord(addr[0]), ord(addr[1]), ord(addr[2]))
 
-def enableCollector():
-    snappyADCEnable()
-    addr = rpcSourceAddr()
-    sendAck(addr, "enableCollector", "-")
-
-def disableCollector():
-    snappyADCDisable()
-    addr = rpcSourceAddr()
-    read_initiated = False
-    sendAck(addr, "disableCollector", "-")
-
-ADC_READY_PIN = 30
-ADC_READY = False
-EVERY_MS = 3
-def initMonitorAdcReady():
-    setPinDir(ADC_READY_PIN, False)
-    monitorPin(ADC_READY_PIN, True)
-    setRate(EVERY_MS)
-
-@setHook(HOOK_GPIN)
-def pinChanged(pinNum, isSet):
-    global read_initiated
-    if not read_initiated:
-        return
-    if pinNum == ADC_READY_PIN and isSet == ADC_READY:
-        readAndPrint2()
-
-def readAndPrint2():
-    global hub_addr
+def readAndSend():
     data = snappySpiRead(3, 8)
     print 'SPI read:', three_bytes_as_text(data)
     sendAck(hub_addr, "readAndPrint", three_bytes_as_text(data))
-
-def readAndPrint():
-    global hub_addr
-    hub_addr = rpcSourceAddr()
-    readAndPrint2()
-
-def initiateRead():
-    global read_initiated
-    read_initiated = True
-    global hub_addr
-    hub_addr = rpcSourceAddr()
-    sendAck(hub_addr, "readInitiated", "-")
 
